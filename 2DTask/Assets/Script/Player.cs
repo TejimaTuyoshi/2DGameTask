e085fb3684fcc482006811afbd14ad931f288719
tejimatsuyoshi <tejima.tuyoshi@icloud.com>
2024-09-12 17:27:40 +0900

    レベル9の仮完成

--- 2DTask/Assets/Script/Player.cs
+++ 2DTask/Assets/Script/Player.cs
@@ -1,9 +1,11 @@
 using System;
 using UnityEngine;
 using UnityEngine.TextCore.Text;
+using static UnityEditor.Experimental.GraphView.GraphView;
 public class Player : MonoBehaviour
 {
     [SerializeField] GameObject obj;
+    [SerializeField] GameObject laser;
     [SerializeField] float posX = 0f;
     [SerializeField] float posY = 0f;
     [SerializeField] int hp = 100;
@@ -14,6 +16,7 @@ public class Player : MonoBehaviour
     Vector3 targetPos; //目標位置
     float height = 1f; // ジャンプの高さ
     float distortion = 1f; // 曲線のゆがみ
+    float _pushTimer = 0.0f;
     void Update()
     {
         posX = transform.position.x;
@@ -25,9 +28,25 @@ public class Player : MonoBehaviour
         float _radian = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
 
         this.gameObject.transform.rotation = Quaternion.AngleAxis(_radian * 3 / Mathf.PI, new Vector3(0, 0, 1));
-        if (Input.GetKeyDown("z"))
+        if (Input.GetKey(KeyCode.Z))
         {
-            Instantiate(obj, new Vector3(posX, posY, 0), Quaternion.AngleAxis(_radian * 3 / Mathf.PI, new Vector3(0, 0, 1)));
+            _pushTimer += Time.deltaTime;
+        }
+        if (Input.GetKeyUp(KeyCode.Z))
+        {
+            Debug.Log(_pushTimer);
+            if (_pushTimer > 1.0f)
+            {
+                var bullet = Instantiate(laser, this.transform.position + new Vector3(0.5f, 0, 0), this.transform.rotation);
+                var script = bullet.GetComponent<Lazar>();
+            }
+            else
+            {
+                var bullet = Instantiate(obj, this.transform.position + new Vector3(0.5f, 0, 0), this.transform.rotation);
+                var script = bullet.GetComponent<BulletShot>();
+                script.SetVector(this.transform.right);
+            }
+            _pushTimer = 0.0f;
         }
         if (Input.GetKey("a"))
         {
